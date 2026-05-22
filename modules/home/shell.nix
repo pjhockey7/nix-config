e7d7a578971779afc156419634ea4ab3cbe47b9b
pj <pjhockey7@gmail.com>
2026-05-22 01:49:04 -0700

    add vim mode to bash

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -19,6 +19,7 @@
 
         initExtra = ''
             export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+            set -o vi
         '';
 
         shellAliases = {
