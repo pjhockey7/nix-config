5a28e23d703c5e4cb9e9dc83b994cc660d12fedd
pjhockey7 <pjhockey7@gmail.com>
2026-06-05 13:57:26 -0700

    more hyprland

--- home-manager/desktops/hyprland.nix
+++ home-manager/desktops/hyprland.nix
@@ -19,14 +19,28 @@ in {
         } ];
       };
 
+      config = {
+        general = {
+	  layout = "master";
+	};
+      };
+
       bind = [
         { _args = [ "${mod} + SHIFT + Return" (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"ghostty\")") ]; }
+        { _args = [ "${mod} + Return" (lib.generators.mkLuaInline "hl.dsp.layout(\"swapwithmaster\")") ]; }
         { _args = [ "${mod} + C" (lib.generators.mkLuaInline "hl.dsp.window.close()") ]; }
         
+	{ _args = [ "${mod} + J" (lib.generators.mkLuaInline "hl.dsp.focus( { direction = \"down\" } )") ]; }
+	{ _args = [ "${mod} + K" (lib.generators.mkLuaInline "hl.dsp.focus( { direction = \"up\" } )") ]; }
+	
+	{ _args = [ "${mod} + L" (lib.generators.mkLuaInline "hl.dsp.layout( \"mfact +0.05\" )") ]; }
+	{ _args = [ "${mod} + H" (lib.generators.mkLuaInline "hl.dsp.layout( \"mfact -0.05\" )") ]; }
+
 	{ _args = [ "${mod} + Q" (lib.generators.mkLuaInline "hl.dsp.exit()") ]; }
       ];
     };
 
+    # Use extra config so that we don't need blocks of 9 for each bind
     extraConfig = ''
       local mod = "${mod}"
 
@@ -35,7 +49,7 @@ in {
 
         hl.bind(mod .. " + " .. key, hl.dsp.focus( { workspace = i } ))
 
-        --hl.bind(mod .. " + SHIFT", key, hl.dsp.movetoworkspace(key))
+        hl.bind(mod .. " + SHIFT + " .. key, hl.dsp.window.move( { workspace = i } ))
       end
     '';
   };
