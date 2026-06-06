da0218433dec34a4daa0e0f0aa9e1e8583c45a33
pjhockey7 <pjhockey7@gmail.com>
2026-06-05 18:52:40 -0700

    add cursor and proper window movement

--- home-manager/desktops/hyprland.nix
+++ home-manager/desktops/hyprland.nix
@@ -10,18 +10,19 @@ in {
     configType = "lua";
 
     settings = {
+
       monitor = {
       	_args = [ {
-	  output = "";
-	  mode = "preferred";
-	  position = "auto";
-	  scale = 1.57;
+          output = "";
+          mode = "preferred";
+          position = "auto";
+          scale = 1.57;
         } ];
       };
 
       config = {
         general = {
-	  layout = "master";
+	        layout = "master";
           gaps_in = 3;
           gaps_out = 5;
           border_size = 2;
@@ -32,7 +33,7 @@ in {
           "col.inactive_border" = "0xaa595959";
           resize_on_border = false;
           allow_tearing = false;
-	};
+	      };
 
         decoration = {
           rounding = 10;
@@ -98,15 +99,15 @@ in {
         { _args = [ "${mod} + Return" (lib.generators.mkLuaInline "hl.dsp.layout(\"swapwithmaster\")") ]; }
         { _args = [ "${mod} + C" (lib.generators.mkLuaInline "hl.dsp.window.close()") ]; }
         
-	    { _args = [ "${mod} + J" (lib.generators.mkLuaInline "hl.dsp.focus( { direction = \"down\" } )") ]; }
-	    { _args = [ "${mod} + K" (lib.generators.mkLuaInline "hl.dsp.focus( { direction = \"up\" } )") ]; }
-	
-	    { _args = [ "${mod} + L" (lib.generators.mkLuaInline "hl.dsp.layout( \"mfact +0.05\" )") ]; }
-	    { _args = [ "${mod} + H" (lib.generators.mkLuaInline "hl.dsp.layout( \"mfact -0.05\" )") ]; }
+        { _args = [ "${mod} + K" (lib.generators.mkLuaInline "hl.dsp.window.cycle_next()") ]; }
+        { _args = [ "${mod} + J" (lib.generators.mkLuaInline "hl.dsp.window.cycle_next({ next = false })") ]; }
+    
+        { _args = [ "${mod} + L" (lib.generators.mkLuaInline "hl.dsp.layout( \"mfact +0.05\" )") ]; }
+        { _args = [ "${mod} + H" (lib.generators.mkLuaInline "hl.dsp.layout( \"mfact -0.05\" )") ]; }
 
         { _args = [ "${mod} + R" (lib.generators.mkLuaInline "hl.dsp.exec_cmd(\"rofi -show drun\")") ]; }
 
-	{ _args = [ "${mod} + Q" (lib.generators.mkLuaInline "hl.dsp.exit()") ]; }
+	      { _args = [ "${mod} + Q" (lib.generators.mkLuaInline "hl.dsp.exit()") ]; }
       ];
     };
 
--- home-manager/home-manager.nix
+++ home-manager/home-manager.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }: {
-    programs.home-manager = {
-        enable = true;
-    };
+  programs.home-manager = {
+    enable = true;
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true; # Uncomment if you use XWayland apps often
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
 }
